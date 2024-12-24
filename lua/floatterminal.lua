b202368ec2808c2583bb64afd89eaeca121807f9
leonardo-luz <leonardoluz.f02@gmail.com>
2024-12-24 12:47:46 -0300

    [fix] opts

--- lua/floatterminal.lua
+++ lua/floatterminal.lua
@@ -18,8 +18,8 @@ end
 local states = {}
 
 ---@class setup.Opts
----@field num integer: Number of floating terminal you want. DEFAULT 3
----@field keymap string: Keymap to open floating terminal, will be your key map plus the number of the terminal, from 1 to num. DEFAULT: <leader>t
+---@field num integer?: Number of floating terminal you want. DEFAULT 3
+---@field keymap string?: Keymap to open floating terminal, will be your key map plus the number of the terminal, from 1 to num. DEFAULT: <leader>t
 
 ---Setup floatterminal plugin
 ---@param opts setup.Opts|nil
@@ -38,10 +38,9 @@ M.setup = function(opts)
 
   for i = 1, (opts.num or 3) do
     vim.keymap.set("n", (opts.keymap or "<leader>t") .. i, function()
-      toggle_terminal(states[i - 1])
+      toggle_terminal(i - 1)
     end, { desc = "[T]oggle terminal [" .. i .. "]" })
   end
-
-  ---@deprecated
-  vim.api.nvim_create_user_command("Floaterminal", M.toggle_terminal, { nargs = 1 })
 end
+
+return M
